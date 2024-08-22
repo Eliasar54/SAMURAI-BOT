feb865e74aacb29045dd888712778823f96c62dc
Eliasar YT <eliasarmoncada4@gmail.com>
2024-08-22 08:36:32 -0600

    Actualizar descargas.js

--- plugins/descargas.js
+++ plugins/descargas.js
@@ -721,8 +721,9 @@ module.exports = { descarga, descarga2}
 
 let file = require.resolve(__filename)
 fs.watchFile(file, () => {
-fs.unwatchFile(file)
-console.log(chalk.redBright(`Update ${__filename}`))
-delete require.cache[file]
-require(file)
+    fs.unwatchFile(file)
+    console.log(chalk.redBright(`Update ${__filename}`))
+    delete require.cache[file]
+    require(file)
 })
+
